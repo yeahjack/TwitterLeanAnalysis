8fe3d286d139f51664f42ad5a2772738f0ebd8a8
yeahjack <yxu409@connect.hkust-gz.edu.cn>
2023-04-28 03:16:18 +0800

    add more comments

--- notebooks/06.Multi-task_Learning/train.py
+++ notebooks/06.Multi-task_Learning/train.py
@@ -9,9 +9,12 @@ from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 from torch.nn.parallel import DataParallel
 from transformers import get_linear_schedule_with_warmup
+
 # Import TensorBoard packages
 from torch.utils.tensorboard import SummaryWriter
 
+
+# Define the custom dataset class
 class NewsDataset(Dataset):
     def __init__(self, data, tokenizer, max_length=512):
         self.data = data
@@ -21,11 +24,13 @@ class NewsDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
+    # Get an item (sample) from the dataset by index
     def __getitem__(self, idx):
         text = self.data.iloc[idx, 0]
         kind = self.data.iloc[idx, 1]
         score = self.data.iloc[idx, 2]
 
+        # Tokenize the text using the tokenizer
         encoding = self.tokenizer(
             text,
             return_tensors="pt",
@@ -36,6 +41,7 @@ class NewsDataset(Dataset):
         input_ids = encoding["input_ids"][0]
         attention_mask = encoding["attention_mask"][0]
 
+        # Return the tokenized input, attention mask, and labels as tensors
         return {
             "input_ids": input_ids,
             "attention_mask": attention_mask,
@@ -46,34 +52,42 @@ class NewsDataset(Dataset):
         }
 
 
+# Define the multitask model class
 class MultiTaskXLMRoberta(nn.Module):
     def __init__(self, cache_dir=None):
         super(MultiTaskXLMRoberta, self).__init__()
+        # Load the pre-trained XLM-RoBERTa model
         self.xlm_roberta = XLMRobertaModel.from_pretrained(
             "xlm-roberta-base", cache_dir=cache_dir
         )
+        # Add a classification head for the "kind" task
         self.classification_head = nn.Linear(self.xlm_roberta.config.hidden_size, 2)
+        # Add a regression head for the "score" task
         self.regression_head = nn.Linear(self.xlm_roberta.config.hidden_size, 1)
 
     def forward(self, input_ids, attention_mask):
+        # Pass the input through the XLM-RoBERTa model
         outputs = self.xlm_roberta(input_ids, attention_mask=attention_mask)
         last_hidden_state = outputs[0]
+        # Extract the [CLS] token representation
         pooled_output = last_hidden_state[:, 0]
 
+        # Pass the [CLS] token representation through the classification and regression heads
         classification_output = self.classification_head(pooled_output)
         regression_output = self.regression_head(pooled_output)
         return classification_output, regression_output.squeeze(-1)
 
 
+# Function to train the model for one epoch
 def train(model, train_loader, optimizer, scheduler, device, epoch, writer):
     model.train()
     total_loss = 0
     classification_loss_fn = nn.CrossEntropyLoss()
     regression_loss_fn = nn.MSELoss()
 
-    # Add 'leave=False' to keep tqdm progress bar on the same line
-    progress_bar = tqdm(train_loader, desc="Training", leave=False)
-    
+    # Use tqdm to show a progress bar for the training loop
+    progress_bar = tqdm(train_loader, leave=False)
+
     for batch_idx, batch in enumerate(progress_bar):
         optimizer.zero_grad()
 
@@ -82,21 +96,29 @@ def train(model, train_loader, optimizer, scheduler, device, epoch, writer):
         kind = batch["kind"].to(device)
         score = batch["score"].to(device)
 
+        # Get the predictions from the model
         kind_preds, score_preds = model(input_ids, attention_mask)
 
+        # Compute the classification and regression losses
         classification_loss = classification_loss_fn(kind_preds, kind)
         regression_loss = regression_loss_fn(score_preds[kind == 1], score[kind == 1])
 
+        # Combine the losses for multitask learning
         loss = classification_loss + regression_loss
         total_loss += loss.item()
 
+        # Perform backpropagation
         loss.backward()
         optimizer.step()
         scheduler.step()
 
         # Log the loss to TensorBoard
-        writer.add_scalar("/home/xuyijie/news-title-bias/notebooks/06.Multi-task_Learning/Loss/train", loss.item(), epoch * len(train_loader) + batch_idx)
-        
+        writer.add_scalar(
+            "/home/xuyijie/news-title-bias/notebooks/06.Multi-task_Learning/Loss/train",
+            loss.item(),
+            epoch * len(train_loader) + batch_idx,
+        )
+
         # Update the tqdm progress bar with the current loss
         progress_bar.set_postfix({"loss": f"{loss.item():.4f}"})
 
@@ -128,15 +150,17 @@ val_dataset = NewsDataset(val_data, tokenizer)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+tqdm.write("Data Loaded!")
 
 # Create a TensorBoard SummaryWriter
-writer = SummaryWriter("/home/xuyijie/news-title-bias/notebooks/06.Multi-task_Learning/runs/multitask_xlm_roberta")
+writer = SummaryWriter(
+    "/home/xuyijie/news-title-bias/notebooks/06.Multi-task_Learning/runs/multitask_xlm_roberta"
+)
 
 # Learning rate scheduling
 warmup_steps = 100  # You can adjust this value
 total_steps = len(train_loader) * epochs
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 # Initialize the model, wrap it with DataParallel, and move it to the device
 model = MultiTaskXLMRoberta(cache_dir=cache_dir)
@@ -161,3 +185,58 @@ for epoch in range(epochs):
 
 # Close the TensorBoard writer
 writer.close()
+
+# The code above defines the multitask learning model using XLM-RoBERTa and trains it for the given number of epochs.
+# The model is saved after each epoch, and training progress is logged using TensorBoard.
+
+# To evaluate the model on the validation set, you can define an evaluation function as follows:
+
+
+def evaluate(model, val_loader, device):
+    model.eval()
+    total_classification_loss = 0
+    total_regression_loss = 0
+    classification_loss_fn = nn.CrossEntropyLoss()
+    regression_loss_fn = nn.MSELoss()
+
+    with torch.no_grad():
+        for batch in val_loader:
+            input_ids = batch["input_ids"].to(device)
+            attention_mask = batch["attention_mask"].to(device)
+            kind = batch["kind"].to(device)
+            score = batch["score"].to(device)
+
+            # Get predictions from the model
+            kind_preds, score_preds = model(input_ids, attention_mask)
+
+            # Compute the classification and regression losses
+            classification_loss = classification_loss_fn(kind_preds, kind)
+            regression_loss = regression_loss_fn(
+                score_preds[kind == 1], score[kind == 1]
+            )
+
+            total_classification_loss += classification_loss.item()
+            total_regression_loss += regression_loss.item()
+
+    avg_classification_loss = total_classification_loss / len(val_loader)
+    avg_regression_loss = total_regression_loss / len(val_loader)
+
+    return avg_classification_loss, avg_regression_loss
+
+
+# Now you can call the evaluate function after training the model and after each epoch:
+
+for epoch in range(epochs):
+    train_loss = train(model, train_loader, optimizer, scheduler, device, epoch, writer)
+    tqdm.write(f"Epoch: {epoch+1}/{epochs}, Loss: {train_loss:.4f}")
+
+    classification_loss, regression_loss = evaluate(model, val_loader, device)
+    tqdm.write(
+        f"Validation: Classification Loss: {classification_loss:.4f}, Regression Loss: {regression_loss:.4f}"
+    )
+
+    # Save the model after each epoch
+    model_to_save = model.module if hasattr(model, "module") else model
+    torch.save(model_to_save.state_dict(), f"output/model_epoch_{epoch+1}.pth")
+
+# This will give you an evaluation of the model's performance on both classification and regression tasks for the validation set.
